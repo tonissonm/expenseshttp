7ecb5041aca4f4a6faa70b3385fd51b58f611fde
Matis Tonisson <matistonisson@gmail.com>
2024-10-06 17:51:28 +0300

    ÜL 4

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,21 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
-function ExpenseItem(props){
-    console.log(props);
 
-    return(<div className='expense-item'>
-            <div>{props.expenseData.date.toString}</div>
-            <div className='expense-item__description'>
-                <h2>{props.expenseData.title}</h2>
-                <div className='expense-item__price'>{props.expenseData.price}</div>
+function ExpenseItem(props){
+
+const day = props.expenseData.date.toLocaleString('en-US', { day: '2-digit' })
+const month = props.expenseData.date.toLocaleString('en-US', { month: 'long' })
+const year = props.expenseData.date.getFullYear()
+    return (
+        <div className='expense-item'>
+            <ExpenseDate
+            date={props.expenseData.date}
+            />
+        <div className="expense-item__description">
+            <h2>{props.expenseData.title}</h2>
+            <div className="expense-item__price">
+            {props.expenseData.price}
+                </div>
             </div>
         </div>
     );
